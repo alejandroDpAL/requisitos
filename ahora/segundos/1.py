41188bc9439462ffe9339cdd3f695929d0872c32
alejandroDpAL <alejandropasaje799@gmail.com>
2023-09-01 14:08:23 -0500

    subir

--- ahora/segundos/1.py
+++ ahora/segundos/1.py
@@ -63,36 +63,3 @@ print("Subtotal:", valor_total)
 print("Descuento:", descuento)
 print("Total con Descuento:", valor_total_descuento)
 print("--------------------------------------------------")
-
-
-
-""" import datetime
-fecha_actual = datetime.date.today()
-
-# Obtener los datos del usuario
-ciudad = input("Ciudad: ")
-nombre_cliente = input("Nombre del cliente: ")
-nombre_producto = input("Nombre del producto: ")
-valor_producto = float(input("Valor del producto: "))
-cantidad_producto = int(input("Cantidad del producto: "))
-
-# Calcular los valores requeridos
-valor_total = valor_producto * cantidad_producto
-descuento = valor_total * 0.175  # Descuento del 17.5%
-valor_total_descuento = valor_total - descuento
-
-# Imprimir la factura de venta
-print("--------------------------------------------------")
-print("           Factura de Venta")
-print("Fecha:", fecha_actual)
-print("Ciudad:", ciudad)
-print("Cliente:", nombre_cliente)
-print("--------------------------------------------------")
-print("Producto:", nombre_producto)
-print("Valor Unitario:", valor_producto)
-print("Cantidad:", cantidad_producto)
-print("--------------------------------------------------")
-print("Subtotal:", valor_total)
-print("Descuento:", descuento)
-print("Total con Descuento:", valor_total_descuento)
-print("--------------------------------------------------") """
